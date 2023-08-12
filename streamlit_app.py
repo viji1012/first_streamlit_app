82d22f864040b6b2244b68e7566feefb5b6794be
viji1012 <102133035+viji1012@users.noreply.github.com>
2023-08-12 12:29:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Wonder Breakfast Menu')
-streamlit.text('Pongal,Sambar,chuttni')
-streamlit.text('Poori,Kilakumasala')
-streamlit.text('Idly,samabr,chuttni')
+streamlit.header('🥣 🥗Breakfast Menu 🥑🍞')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
