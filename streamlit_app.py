2f38c52068b6d86df034743c8bbbcf784804dbde
viji1012 <102133035+viji1012@users.noreply.github.com>
2023-08-12 12:18:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Wonder Breakfast Menu')
+streamlit.text('Pongal,Sambar,chuttni')
+streamlit.text('Poori,Kilakumasala')
+streamlit.text('Idly,samabr,chuttni')
+
