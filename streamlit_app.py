12e231831ccac1de99174d209ea643bbb691a9f1
viji1012 <102133035+viji1012@users.noreply.github.com>
2023-08-21 13:35:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-
+snowflake.connector("requriment.txt")
 
 
 
